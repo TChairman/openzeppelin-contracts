ea77b8fcce6fe0d285d6cdd1f000f6e0be0cd235
The Chairman <tchairman0@gmail.com>
2022-12-08 16:27:34 -0800

    Created ERC4626Accounting.sol

---
+++ contracts/token/ERC20/extensions/ERC4626Accounting.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./ERC4626.sol";
+import "../../../interfaces/IERC20.sol";
+import "../../../utils/math/Math.sol";
+/**
+ * @dev Extension of the ERC4626 "Tokenized Vault Standard" as defined in
+ * https://eips.ethereum.org/EIPS/eip-4626[EIP-4626].
+ *
+ * This extenision proviedes basic balance sheet accounting functions that are required
+ * for many real-world implementations of ERC4626 vaults, including:
+ * - totalEquity()
+ * - totalLiabilities() and
+ * - totalNAV()
+ * 
+ * This implementation redefines the share price functions to be in terms of totalEquity() instead of totalAssets()
+ *
+ * CAUTION: see ERC4626.sol to learn about the donation attack and potential mitigations
+ *
+ */
+abstract contract ERC4626Acounting is ERC4626 {
+    using Math for uint256;
+    
+    /** @dev See {IERC4626-totalAssets}. */
+    function totalAssets() public view virtual override returns (uint256) {
+        return _availableAssets() + totalNAV();
+    }
+
+     // total asset value of outside investments
+    function totalNAV() public virtual view returns (uint256) {
+        return 0;
+    }
+
+    // total liabilities, e.g. accrued fees, or loans to the vault
+    function totalLiabilities() public view virtual returns (uint256) {
+        return 0;
+    }
+
+    // total equity value of the shareholders
+    function totalEquity() public view virtual returns (uint256) {
+        uint256 assets = totalAssets();
+        uint256 liabilities = totalLiabilities();
+        return (liabilities > assets) ? 0 : assets - liabilities;
+    }
+
+    // available assets should be actual liquid assets that are not reserved for something else, e.g. capital call or planned redemption
+    function _availableAssets() internal virtual view returns (uint256) {
+        return IERC20(asset()).balanceOf(address(this));
+    }
+    
+    /**
+     * @dev Internal conversion function (from assets to shares) with support for rounding direction. Usees totalEquity() instead of totalAssets().
+     *
+     * Will revert if assets > 0, totalSupply > 0 and totalAssets = 0. That corresponds to a case where any asset
+     * would represent an infinite amount of shares.
+     */
+    function _convertToShares(uint256 assets, Math.Rounding rounding) internal view virtual override returns (uint256) {
+        uint256 supply = totalSupply();
+        return
+            (assets == 0 || supply == 0)
+                ? _initialConvertToShares(assets, rounding)
+                : assets.mulDiv(supply, totalEquity(), rounding);
+    }
+
+    /**
+     * @dev Internal conversion function (from shares to assets) with support for rounding direction. Usees totalEquity() instead of totalAssets().
+     */
+    function _convertToAssets(uint256 shares, Math.Rounding rounding) internal view virtual override returns (uint256) {
+        uint256 supply = totalSupply();
+        return
+            (supply == 0) ? _initialConvertToAssets(shares, rounding) : shares.mulDiv(totalEquity(), supply, rounding);
+    }
+
+    // put in some useful hooks for fees and other things
+    function _beforeWithdraw(address owner, uint256 assets, uint256 shares) internal virtual {}
+
+    function _afterDeposit(address receiver, uint256 assets, uint256 shares) internal virtual {}
+
+    function _withdraw (
+        address caller,
+        address receiver,
+        address owner,
+        uint256 assets,
+        uint256 shares
+    ) internal virtual override {
+        _beforeWithdraw(owner, assets, shares);
+        super._withdraw(caller, receiver, owner, assets, shares);
+    }
+
+    function _deposit (
+        address caller,
+        address receiver,
+        uint256 assets,
+        uint256 shares
+    ) internal virtual override {
+        super._deposit(caller, receiver, assets, shares);
+        _afterDeposit(receiver, assets, shares);
+    }
+
+}
